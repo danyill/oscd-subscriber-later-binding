3d6abfbacbe7b877f683700ae4ef3ebc5f6e0185
danyill <danyill@users.noreply.github.com>
2023-04-15 03:54:39 +0000

    Deploying to gh-pages from @ danyill/oscd-subscriber-later-binding@c004dd081ca8e9ddca91d120ad44824d188e3c1e 🚀

---
+++ foundation/foundation.d.ts
@@ -0,0 +1,27 @@
+import { Update } from '@openscd/open-scd-core';
+export declare type SclEdition = '2003' | '2007B' | '2007B4';
+export declare function getSclSchemaVersion(doc: Document): SclEdition;
+export declare const serviceTypes: Partial<Record<string, string>>;
+/**
+ * Extract the 'name' attribute from the given XML element.
+ * @param element - The element to extract name from.
+ * @returns the name, or undefined if there is no name.
+ */
+export declare function getNameAttribute(element: Element): string | undefined;
+/**
+ * Extract the 'desc' attribute from the given XML element.
+ * @param element - The element to extract description from.
+ * @returns the name, or undefined if there is no description.
+ */
+export declare function getDescriptionAttribute(element: Element): string | undefined;
+export declare function isPublic(element: Element): boolean;
+/**
+ * @param lnElements - The LN elements to be scanned for `inst`
+ * values already in use.
+ * @returns first available inst value for LN or undefined if no inst is available
+ */
+export declare function minAvailableLogicalNodeInstance(lnElements: Element[]): string | undefined;
+export declare function createUpdateEdit(element: Element, attributes: Record<string, string | null>): Update;
+/** Sorts selected `ListItem`s to the top and disabled ones to the bottom. */
+export declare function compareNames(a: Element | string, b: Element | string): number;
+export declare function findFCDAs(extRef: Element): Element[];
---
+++ locales/de.d.ts
@@ -0,0 +1,18 @@
+export declare const templates: {
+    s182ac4e81ae15024: string;
+    s1cff131b3b4aa852: string;
+    s337459c5378dc0dc: string;
+    s392d68658d0c0a7b: string;
+    s4d958711afb20742: string;
+    s54431d00a8030125: string;
+    s7b172c6f7ac1cb0c: string;
+    s7c95876637f6a9d7: string;
+    s8593cda43a906b03: string;
+    s94915263ea5e8caf: string;
+    s9d8b8aa2b404c2c8: string;
+    sd099c72448b745ed: string;
+    se275ef18f06cf8eb: string;
+    se7a38a6492965c7a: string;
+    sea5933c70ba5a518: string;
+    sf5bd9968dcf60b09: string;
+};
