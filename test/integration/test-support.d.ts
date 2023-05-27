de72e77c64fa4082de9e86b8b2a3554ee70a82bc
danyill <danyill@users.noreply.github.com>
2023-05-27 21:23:28 +0000

    Deploying to gh-pages from @ danyill/oscd-subscriber-later-binding@d20a91fc7a4d3a6c17de6354b6593c617f2ef0fa 🚀

--- test/integration/test-support.d.ts
+++ test/integration/test-support.d.ts
@@ -1,5 +1,6 @@
 import { ListItemBase } from '@material/mwc-list/mwc-list-item-base';
 import { OscdFilteredList } from '@openscd/oscd-filtered-list';
+export declare function tryViewportSet(): Promise<void>;
 export declare function midEl(element: Element): [number, number];
 export declare function getFcdaItem(listElement: OscdFilteredList, controlIdentity: string, fcdaIdentity: string): ListItemBase | null;
 export declare function getExtRefItem(listElement: OscdFilteredList, extRefId: string): ListItemBase | null;
